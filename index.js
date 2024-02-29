f3029464fe07f3d4bd5051a2368a90b2cc2baccf
alexander perlov <perlov3301@gmal.com>
2024-02-29 06:25:31 +0200

    status(400) member not found

--- index.js
+++ index.js
@@ -7,12 +7,34 @@ import logger from './middleware/logger.js';
 const app = express();
 
 // init middleware
-app.use(logger);
-// route for dynamicly serving to get all
+// app.use(logger);
+
+// route for dynamicly serving to get all members
 app.get('/api/members', (req,res)=>{
     // express take care about stringify
     res.json(members); 
 });
+
+// get single member with url parameter 'id'
+app.get(`/api/members/:id`, (req,res)=>{
+    // we can use req Object to get id
+    const allpar = req.params;
+    const aId = allpar.id;
+    const idNumber = parseInt(req.params.id);
+    console.log(`indexjs: parse(id): ${idNumber} of type ${typeof idNumber}`);
+    // members.forEach(obj=>{ console.log(obj);});
+    let mysingle = {};
+    const found = members.some(member=> member.id===idNumber);//true or false
+    if (found) {
+      const myfilter = members.filter((obj)=> obj.id===idNumber );
+      mysingle = myfilter[0];
+      console.log(`mysingle id:${mysingle.id}, name:${mysingle.name}`);
+      res.json(mysingle);
+    } else { 
+        res.status(400).json({msg:`Member with id ${idNumber} not found`});
+     }
+    
+}) ;
 // creting of route for static serving
 const pubFolder = path.join(myDir(),'public');
 app.use(express.static(pubFolder));
--- middleware/logger.js
+++ middleware/logger.js
@@ -1,12 +1,12 @@
 import moment from  './moment.js';
 // middleware, that can manage req and/or res
 const logger = (req,res,next) => {
-    console.log(moment());
+    console.log(`logger fired: ${moment()}`);
     // each and every time I send request through postman
     console.log('request has been send');
     console.log(`protocol of request:${req.protocol}`);
     console.log(`hosts parts of url:${req.get('host')}`);
-    console.log(`originalUrl of request: ${req.originalUrl} ${moment()}`);
+    console.log(`originalUrl of request: ${req.originalUrl} `);
     next();
 }
 export default logger;
